c162610b61a754bbe03f3e819e3270673460f952
ali85k <aliceanndraad56@gmail.com>
2022-06-29 21:08:26 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = "Alice"
+console.log(nome)
+
+ let num = 5
+console.log(num)
+
+ let anoAtual = 2022 
+ let resultado = anoAtual - num
+ console.log(resultado)
+
+console.log(anoAtual + num)
+
+let mensagem = "Estou mexendo no console.log"
+
+let quartaFeira = false
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
